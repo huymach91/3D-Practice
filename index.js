d4f2a5a8201d29f10862325af62a808b198b0d61
huymach91 <huy.mach.webdev@gmail.com>
2022-04-10 15:57:23 +0700

    1 file modified

--- index.js
+++ index.js
@@ -15,16 +15,25 @@ const scene = new THREE.Scene();
 // axis helper
 const axisHelper = new THREE.AxesHelper();
 scene.add(axisHelper);
+// texture
+const textureLoader = new THREE.TextureLoader();
+const textureDoor = textureLoader.load(
+  'https://raw.githubusercontent.com/huymach91/3D-Practice/master/images/color.jpg'
+);
 
 // objects
 const geometry = new THREE.BoxGeometry(1, 1, 1);
+const sphereGeometry = new THREE.SphereBufferGeometry(0.5, 10, 10);
 const material = new THREE.MeshBasicMaterial({
   color: 0x00ff00,
+  map: textureDoor,
 });
 const cube = new THREE.Mesh(geometry, material);
 cube.scale.x = 0.5;
+const sphere = new THREE.Mesh(sphereGeometry, material);
+sphere.position.x = 1.5;
 
-scene.add(cube);
+scene.add(cube, sphere);
 
 // tween
 
@@ -55,11 +64,15 @@ window.addEventListener('dblclick', () => {
   }
 });
 
+const clock = new THREE.Clock();
+
 const stick = () => {
   // camera.lookAt(cube.position);
+  const elapsedTime = clock.getElapsedTime();
+  sphere.rotation.y = 0.5 * elapsedTime;
+  cube.rotation.y = 0.5 * elapsedTime;
 
   renderer.render(scene, camera);
-
   window.requestAnimationFrame(stick);
 };
 
