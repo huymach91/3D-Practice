8245e32524fec0032e82f32ffd87ab019ebbd175
huymach91 <huy.mach.webdev@gmail.com>
2022-05-02 10:32:08 +0700

    1 file modified

--- index.js
+++ index.js
@@ -5,110 +5,57 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'lil-gui';
 
-const gui = new dat.GUI();
-
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
+const size = {
+  width: 1200,
+  height: 800,
 };
 
 // scene
 const scene = new THREE.Scene();
-// axis helper
-const axisHelper = new THREE.AxesHelper();
-// texture
-const textureLoader = new THREE.TextureLoader();
-const simpleShadow = textureLoader.load(
-  'https://raw.githubusercontent.com/huymach91/3D-Practice/master/images/simpleShadow.jpg'
-);
+const canvas = document.getElementById('canvas');
 
-// objects
-const material = new THREE.MeshStandardMaterial();
-// material.roughness = 0.7;
+// material
+const basicMaterial = new THREE.MeshBasicMaterial({
+  color: 0xffff00,
+  side: THREE.DoubleSide,
+});
 
-const sphere = new THREE.Mesh(new THREE.SphereGeometry(1), material);
-sphere.position.y = 0.5;
-sphere.castShadow = true;
+// object
+const planeGeometry = new THREE.PlaneGeometry(1, 1);
+const plane = new THREE.Mesh(planeGeometry, basicMaterial);
+plane.position.set(0, 0, 0);
+plane.rotation.x = -Math.PI;
+plane.rotation.y = Math.PI;
 
-const torus = new THREE.Mesh(new THREE.TorusGeometry(1), material);
-torus.position.y = 0.5;
-torus.position.x = 4;
-torus.castShadow = true;
-
-const plane = new THREE.Mesh(new THREE.PlaneGeometry(10, 10), material);
-plane.rotation.x = -Math.PI * 0.5;
-plane.position.y = -0.5;
-plane.receiveShadow = true;
-
-scene.add(sphere, plane, torus);
-
-// lights
-
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
-ambientLight.position.x = 1;
-
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-directionalLight.position.y = 4;
-directionalLight.position.x = 10;
-directionalLight.position.z = 5;
-
-directionalLight.castShadow = true;
-
-directionalLight.shadow.camera.near = 5;
-directionalLight.shadow.camera.far = 20;
-directionalLight.shadow.mapSize.width = 500;
-directionalLight.shadow.mapSize.height = 500;
-
-gui.add(directionalLight.shadow.camera, 'near').min(0).max(10).step(1);
-
-// scene.add(spotLight);
-scene.add(ambientLight);
-scene.add(directionalLight);
+// ambient light
+const ambientLight = new THREE.AmbientLight();
 
 // camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.y = 5;
-camera.position.x = 1;
-camera.position.z = 20;
+const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
+camera.position.z = 5;
+camera.lookAt(plane.position);
 
-scene.add(camera);
-scene.add(new THREE.CameraHelper(directionalLight.shadow.camera));
+// controls
+const orbitControl = new OrbitControls(camera, canvas);
 
-// canvas
-const canvas = document.getElementById('canvas');
+// camera helper
+const axisHelper = new THREE.AxesHelper();
+
+// add to scene
+scene.add(plane, camera, ambientLight, axisHelper);
+
+// renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
-renderer.setSize(sizes.width, sizes.height);
-renderer.setClearColor(0x000000, 1);
-
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-
+renderer.setSize(size.width, size.height);
 renderer.render(scene, camera);
 
-const controls = new OrbitControls(camera, canvas);
-
-window.addEventListener('dblclick', () => {
-  if (!document.fullscreenElement) {
-    canvas.requestFullscreen();
-  } else {
-    document.exitFullscreen();
-  }
-});
-
-const clock = new THREE.Clock();
-
-const stick = () => {
-  directionalLight.position.x = Math.sin(clock.getElapsedTime()) * 5.5;
-
-  controls.update();
+// animate
+const tick = () => {
+  orbitControl.update();
+  window.requestAnimationFrame(tick);
   renderer.render(scene, camera);
-  window.requestAnimationFrame(stick);
 };
 
-stick();
-
-gui.add(material, 'metalness').min(0).max(1).step(0.0001);
-gui.add(material, 'roughness').min(0).max(1).step(0.0001);
-gui.add(material, 'aoMapIntensity').min(0).max(1).step(0.1);
+tick();
