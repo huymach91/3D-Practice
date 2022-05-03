dce8c25a8b3072c052214f9163143679fb82d11a
huymach91 <huy.mach.webdev@gmail.com>
2022-05-03 16:24:24 +0700

    1 file modified

--- grave.js
+++ grave.js
@@ -7,13 +7,13 @@ const graves = [];
 
 for (var i = 1; i < 30; i++) {
   const angle = Math.random() * Math.PI * 2;
-  const x = Math.sin(angle);
-  const y = Math.cos(angle);
+  const radius = 1.2 * Math.random() * 6;
+  const x = Math.sin(angle) * radius;
+  const z = Math.cos(angle) * radius;
 
   const graveMesh = new THREE.Mesh(boxGeometry, material);
-  graveMesh.position.x = x;
-  graveMesh.position.y = 2;
-  graveMesh.scale.set(0.5, 0.5, 0.5);
+  graveMesh.position.set(x, -0.2, z);
+  graveMesh.scale.set(0.6, 0.8, 0.2);
 
   graves.push(graveMesh);
 }
