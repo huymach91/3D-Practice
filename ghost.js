6c0bbbb63b18e0788996c5ad1f5121bbca997853
huymach91 <huy.mach.webdev@gmail.com>
2022-05-31 07:10:41 +0700

    1 file modified

--- ghost.js
+++ ghost.js
@@ -1,6 +1,15 @@
 import * as THREE from 'three';
+import gsap from 'gsap';
 
-const ghost = new THREE.PointLight(0xffffff, 1, 100);
-ghost.position.set(1, 1, 2);
+const ghost = new THREE.PointLight(0xffffff, 0.85, 50);
+ghost.position.set(1, 1, 6);
+
+gsap.to(ghost.position, {
+  duration: 1,
+  delay: 1,
+  y: 5,
+  yoyo: true,
+  repeat: -1,
+});
 
 export default ghost;
